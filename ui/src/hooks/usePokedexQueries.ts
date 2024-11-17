0b677e909bbe00b182ce0df488ec407bf0e71bc3
JIDR <velanor@gmail.com>
2024-11-17 22:22:41 +0100

    First Commit

---
+++ ui/src/hooks/usePokedexQueries.ts
@@ -0,0 +1,18 @@
+import {useQuery} from '@tanstack/react-query'
+import {getPokemons} from '../services'
+import {QUERY_KEYS} from '../constants'
+
+export const usePokedexQueries = () => {
+  const {data, isError, isLoading} = useQuery({
+    queryKey: [QUERY_KEYS.pokedex],
+    queryFn: () => getPokemons(),
+    staleTime: 1000 * 60,
+    networkMode: 'always'
+  })
+
+  return {
+    isError,
+    isLoading,
+    pokemons: data
+  }
+}
